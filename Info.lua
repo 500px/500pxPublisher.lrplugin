6957301d3fcbd69769e208805ce7ab29059b5b74
Kevin Martin <martin.kevin@gmail.com>
2014-07-18 15:39:25 -0400

    Increment version number.

--- Info.lua
+++ Info.lua
@@ -12,5 +12,5 @@ return {
 	},
 	LrMetadataTagsetFactory = "500pxTagset.lua",
 	LrMetadataProvider = "500pxMetadataDefinition.lua",
-	VERSION = { major=1, minor=2 },
+	VERSION = { major=1, minor=4 },
 }
