551c98df6fafd7c9cc25b30a46ef7efeeb95f207
Akbar Nurlybayev <akbar.nur@gmail.com>
2015-10-14 15:51:21 -0400

    Bump version to 1.9

--- Info.lua
+++ Info.lua
@@ -12,6 +12,6 @@ return {
 	},
 	LrMetadataTagsetFactory = "500pxTagset.lua",
 	LrMetadataProvider = "500pxMetadataDefinition.lua",
-	VERSION = { major=1, minor=8 },
+	VERSION = { major=1, minor=9 },
 	URLHandler = "500pxURLHandler.lua",
 }
